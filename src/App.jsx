7679f58b96b7e789acdd7cf66813cecae7c146cc
Matdoc <antonyvishal1989@gmail.com>
2024-12-07 02:05:16 +0530

    first initial push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TextSearch from './component/search/TextSearch'
+ 
+function App() {
+  return (
+   <div>
+    <TextSearch/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/component/search/TextSearch.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+// here will search the text , need to support partial , full text ,  case  and also we need to handle the error
+
+const data = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
+
+function TextSearch() {
+  const [text, setText] = useState("hi bro");
+  const [result , setResult] = useState();
+
+  // basics tructure is completed
+  const findData = (e) => {
+    e.preventDefault();
+    const found = data.find((value) => text.toLowerCase() === value.word.toLowerCase());
+
+    if (found) {
+      setResult(found.meaning);
+    }else{
+      setResult("Word not found in the dictionary.")
+    }
+
+  };
+
+  return (
+    <div>
+      <h1>Dictionary App</h1>
+      <form onSubmit={findData}>
+        <label htmlFor="search"></label>
+        <input
+          type="text"
+          id="search"
+          placeholder="Enter a word..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <button type="submit">Search</button>
+      </form>
+
+      <h2>Definition : </h2>
+      <h2>{result}</h2>
+    </div>
+  );
+}
+
+export default TextSearch;
